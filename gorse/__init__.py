0aa63f5f33fc9d9c123b5e7ce9fbe1d5a0f8b70e
Zhenghao Zhang <zhangzhenghao@hotmail.com>
2022-10-14 20:13:12 +0800

    replace BaseException with Exception

--- gorse/__init__.py
+++ gorse/__init__.py
@@ -16,7 +16,7 @@ from typing import List, Tuple
 import requests
 
 
-class GorseException(BaseException):
+class GorseException(Exception):
     def __init__(self, status_code: int, message: str):
         self.status_code = status_code
         self.message = message
