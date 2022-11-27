f228fa905b6fb8a3db6b4054a7f8f064a544dd80
zhenghaoz <zhangzhenghao@hotmail.com>
2022-11-27 07:48:31 +0000

    refactor http request

--- gorse/__init__.py
+++ gorse/__init__.py
@@ -23,16 +23,20 @@ class GorseException(Exception):
 
 
 class Gorse:
-    def __init__(self, entry_point: str, api_key: str):
+    def __init__(self, entry_point: str, api_key: str, timeout=None):
         self.entry_point = entry_point
         self.api_key = api_key
+        self.timeout = timeout
 
     def insert_feedback(
             self, feedback_type: str, user_id: str, item_id: str, timestamp: str
     ) -> dict:
-        r = requests.post(
-            self.entry_point + "/api/feedback",
-            headers={"X-API-Key": self.api_key},
+        """
+        Insert a feedback.
+        """
+        return self.__request(
+            "POST",
+            f"{self.entry_point}/api/feedback",
             json=[
                 {
                     "FeedbackType": feedback_type,
@@ -42,80 +46,54 @@ class Gorse:
                 }
             ],
         )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
 
     def list_feedbacks(self, feedback_type: str, user_id: str):
-        r = requests.get(self.entry_point + "/api/user/" + user_id + "/feedback/" + feedback_type,
-                         headers={"X-API-Key": self.api_key})
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        List feedbacks from a user.
+        """
+        return self.__request("GET", f"{self.entry_point}/api/user/{user_id}/feedback/{feedback_type}")
 
     def get_recommend(self, user_id: str, category: str = "", n: int = 10, offset: int = 0, write_back_type: str = None,
                       write_back_delay: str = None) -> List[str]:
+        """
+        Get recommendation.
+        """
         payload = {"n": n, "offset": offset}
         if write_back_type:
             payload["write-back-type"] = write_back_type
         if write_back_delay:
             payload["write-back-delay"] = write_back_delay
-        r = requests.get(
-            self.entry_point + "/api/recommend/%s/%s" % (user_id, category),
-            params=payload,
-            headers={"X-API-Key": self.api_key},
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        return self.__request("GET", f"{self.entry_point}/api/recommend/{user_id}/{category}", params=payload)
 
     def session_recommend(self, feedbacks: list, n: int = 10) -> list:
-        r = requests.post(
-            self.entry_point + "/api/session/recommend?n=%d" % n,
-            headers={"X-API-Key": self.api_key},
-            json=feedbacks,
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Get session recommendation.
+        """
+        return self.__request("POST", f"{self.entry_point}/api/session/recommend?n={n}", json=feedbacks)
 
     def get_neighbors(self, item_id: str, n: int = 10, offset: int = 0) -> List[str]:
-        r = requests.get(
-            self.entry_point + "/api/item/%s/neighbors?n=%d&offset=%d" % (item_id, n, offset),
-            headers={"X-API-Key": self.api_key},
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Get item neighbors.
+        """
+        return self.__request("GET", f"{self.entry_point}/api/item/{item_id}/neighbors?n={n}&offset={offset}")
 
     def insert_feedbacks(self, feedbacks: list) -> dict:
-        r = requests.post(
-            self.entry_point + "/api/feedback",
-            headers={"X-API-Key": self.api_key},
-            json=feedbacks,
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Insert feedbacks.
+        """
+        return self.__request("POST", f"{self.entry_point}/api/feedback", json=feedbacks)
 
     def insert_item(self, item) -> dict:
-        r = requests.post(
-            self.entry_point + "/api/item",
-            headers={"X-API-Key": self.api_key},
-            json=item,
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Insert an item.
+        """
+        return self.__request("POST", f"{self.entry_point}/api/item", json=item)
 
     def get_item(self, item_id: str) -> dict:
-        r = requests.get(
-            self.entry_point + "/api/item/%s" % item_id,
-            headers={"X-API-Key": self.api_key},
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Get an item.
+        """
+        return self.__request("GET", f"{self.entry_point}/api/item/{item_id}")
 
     def get_items(self, n: int, cursor: str = '') -> Tuple[List[dict], str]:
         """
@@ -124,60 +102,56 @@ class Gorse:
         :param cursor: cursor for next page
         :return: items and cursor for next page
         """
-        r = requests.get(self.entry_point + "/api/items", params={'n': n, 'cursor': cursor},
-                         headers={"X-API-Key": self.api_key})
-        if r.status_code == 200:
-            json = r.json()
-            return json['Items'], json['Cursor']
-        raise GorseException(r.status_code, r.text)
+        response = self.__request(
+            "GET", f"{self.entry_point}/api/items", params={'n': n, 'cursor': cursor})
+        return response['Items'], response['Cursor']
 
     def update_item(self, item_id: str, is_hidden: bool = None, categories: List[str] = None, labels: List[str] = None,
                     timestamp: str = None,
                     comment: str = None) -> dict:
-        r = requests.patch(self.entry_point + '/api/item/%s' % item_id, headers={"X-API-Key": self.api_key}, json={
+        """
+        Update an item.
+        """
+        return self.__request("PATCH", f'{self.entry_point}/api/item/{item_id}', json={
             "Categories": categories,
             "Comment": comment,
             "IsHidden": is_hidden,
             "Labels": labels,
             "Timestamp": timestamp
         })
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
 
     def delete_item(self, item_id: str) -> dict:
-        r = requests.delete(
-            self.entry_point + "/api/item/%s" % item_id,
-            headers={"X-API-Key": self.api_key},
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Delete an item.
+        """
+        return self.__request("DELETE", f"{self.entry_point}/api/item/{item_id}")
 
     def insert_user(self, user) -> dict:
-        r = requests.post(
-            self.entry_point + "/api/user",
-            headers={"X-API-Key": self.api_key},
-            json=user,
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Insert a user.
+        """
+        return self.__request("POST", f"{self.entry_point}/api/user", json=user)
 
     def get_user(self, user_id: str) -> dict:
-        r = requests.get(
-            self.entry_point + "/api/user/%s" % user_id,
-            headers={"X-API-Key": self.api_key},
-        )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        """
+        Get a user.
+        """
+        return self.__request("GET", f"{self.entry_point}/api/user/{user_id}")
 
     def delete_user(self, user_id: str) -> dict:
-        r = requests.delete(
-            self.entry_point + "/api/user/%s" % user_id,
+        """
+        Delete a user.
+        """
+        return self.__request("DELETE", f"{self.entry_point}/api/user/{user_id}")
+
+    def __request(self, method: str, url: str, params=None, json=None) -> dict:
+        response = requests.request(
+            method, url,
+            params=params,
             headers={"X-API-Key": self.api_key},
+            timeout=self.timeout,
+            json=json
         )
-        if r.status_code == 200:
-            return r.json()
-        raise GorseException(r.status_code, r.text)
+        if response.status_code == 200:
+            return response.json()
+        raise GorseException(response.status_code, response.text)
